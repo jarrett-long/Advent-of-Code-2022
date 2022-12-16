26e174f5f035a3a3aaeea322302424e28a2be2ef
Jarrett Long <jlong@rightpoint.com>
2022-12-16 17:44:23 -0600

    day04

---
+++ day04/part1.js
@@ -0,0 +1,21 @@
+import { readLines } from '../utils/input-helper.js'
+export default (input) => {
+  const getRange = (str) => {
+    const [start, end] = str.split('-').map((c) => parseInt(c))
+    return { start, end }
+  }
+
+  const fullyContains = ({ r1, r2 }) => {
+    return (
+      (r1.start <= r2.start && r1.end >= r2.end) ||
+      (r2.start <= r1.start && r2.end >= r1.end)
+    )
+  }
+
+  const total = readLines(input)
+    .map((line) => line.split(','))
+    .map((pairArr) => ({ r1: getRange(pairArr[0]), r2: getRange(pairArr[1]) }))
+    .reduce((count, pair) => (fullyContains(pair) ? ++count : count), 0)
+
+  return `Total full overlaps: ${total}`
+}
---
+++ day04/part2.js
@@ -0,0 +1,18 @@
+import { readLines } from '../utils/input-helper.js'
+export default (input) => {
+  const getRange = (str) => {
+    const [start, end] = str.split('-').map((c) => parseInt(c))
+    return { start, end }
+  }
+
+  const overlap = ({ r1, r2 }) => {
+    return !(r1.end < r2.start || r2.end < r1.start)
+  }
+
+  const total = readLines(input)
+    .map((line) => line.split(','))
+    .map((pairArr) => ({ r1: getRange(pairArr[0]), r2: getRange(pairArr[1]) }))
+    .reduce((count, pair) => (overlap(pair) ? ++count : count), 0)
+
+  return `Total partial overlaps: ${total}`
+}
