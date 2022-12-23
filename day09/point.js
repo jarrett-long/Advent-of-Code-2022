ade644782319383aaca41f811d0586898b3e2e2c
Jarrett Long <jlong@rightpoint.com>
2022-12-22 22:59:19 -0600

    day09

--- day09/point.js
+++ day09/point.js
@@ -7,78 +7,28 @@ export default class Point {
 
   pos = () => `(${this.x},${this.y})`
 
-  up(amt) {
-    this.y += amt
-  }
-
-  right(amt) {
-    this.x += amt
-  }
-
-  down(amt) {
-    this.y -= amt
-  }
-
-  left(amt) {
-    this.x -= amt
+  move = {
+    U: (amt) => (this.y += amt),
+    D: (amt) => (this.y -= amt),
+    L: (amt) => (this.x -= amt),
+    R: (amt) => (this.x += amt),
   }
 
   step(dir) {
-    switch (dir) {
-      case 'U':
-        this.up(1)
-        break
-      case 'R':
-        this.right(1)
-        break
-      case 'D':
-        this.down(1)
-        break
-      case 'L':
-        this.left(1)
-        break
-      case 'UR':
-        this.up(1)
-        this.right(1)
-        break
-      case 'UL':
-        this.up(1)
-        this.left(1)
-        break
-      case 'DR':
-        this.down(1)
-        this.right(1)
-        break
-      case 'DL':
-        this.down(1)
-        this.left(1)
-        break
-    }
+    dir.split('').forEach((d) => {
+      this.move[d](1)
+    })
     this.history.add(this.pos())
   }
 
   dirOf(point) {
-    let dir = ''
-
     const yDist = point.y - this.y
-    if (yDist != 0) {
-      if (yDist > 0) {
-        dir += 'U'
-      } else {
-        dir += 'D'
-      }
-    }
+    const yDir = yDist > 0 ? 'U' : yDist < 0 ? 'D' : ''
 
     const xDist = point.x - this.x
-    if (xDist != 0) {
-      if (xDist > 0) {
-        dir += 'R'
-      } else {
-        dir += 'L'
-      }
-    }
+    const xDir = xDist > 0 ? 'R' : xDist < 0 ? 'L' : ''
 
-    return dir
+    return yDir + xDir
   }
 
   isAdjTo(point) {
