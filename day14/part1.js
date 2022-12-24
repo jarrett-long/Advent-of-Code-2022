765752ebce7a972a0036abe33b22e667d2af4581
Jarrett Long <jlong@rightpoint.com>
2022-12-24 15:34:31 -0600

    day14

---
+++ day14/part1.js
@@ -0,0 +1,4 @@
+export default (input) => {
+  // have at it
+  return `part`
+}
--- debug.js
+++ debug.js
@@ -1,2 +1,2 @@
 import { run } from './run.js'
-const answer = await run('13', '2', 'sample')
+const answer = await run('14', '1', 'sample')
