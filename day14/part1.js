5e65846ea0298068374bb00d5fe6d73df862e4a6
Jarrett <jarrett.a.long@gmail.com>
2022-12-24 16:47:21 -0600

    Line endings

--- day14/part1.js
+++ day14/part1.js
@@ -1,4 +1,19 @@
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
 export default (input) => {
+  const rockPoints = input.split('\n').map((line) =>
+    line.split(' -> ').map((p) => {
+      let [x, y] = p.split(',')
+      return new Point(x, y)
+    })
+  )
+
+  let hi = rockPoints
   // have at it
   return `part`
 }
--- debug.js
+++ debug.js
@@ -1,2 +1,2 @@
 import { run } from './run.js'
-const answer = await run('14', '1', 'sample')
+const answer = await run('01', '1', 'input')
